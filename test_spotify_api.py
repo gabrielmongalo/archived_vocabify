5a6e9f071b542c6c26455b917723474e735eb590
Gabriel Alexander Mongalo <gmongalo90@gmail.com>
2019-11-11 22:45:31 -0700

    saving progress and retreaving album information

--- test_spotify_api.py
+++ test_spotify_api.py
@@ -21,3 +21,52 @@ except:
 # Create Spotify Object
 
 spotifyObject = spotipy.Spotify(auth=token)
+
+user = spotifyObject.current_user()
+
+displayName = user['display_name']
+followers = user['followers']['total']
+
+while True:
+    print()
+    print(">>>>>> Welcome to Vocabify " + displayName + "!")
+    print(">>>>>> You have " + str(followers) + "followers.")
+    print()
+    print("0 -- Search for an artists")
+    print("1 -- Exit")
+    print()
+    choice = input("Your choice: ")
+
+# Search for the artist
+    if choice == "0":
+        print("")
+        searchQuery = input("Ok, what's their name?: ")
+        print()
+
+        # Get the search results
+        searchResults = spotifyObject.search(searchQuery, 1, 0, 'artist')
+        print(json.dumps(searchResults, sort_keys=True, indent=4))
+
+        # Artist Details
+        artist = searchResults['artists']['items'][0]
+        print(artist['name'])
+        print(str(artist['followers']['total']) + ' followers')
+        print('Genre: ' + artist['genres'][0])
+        webbrowser.open(artist['images'][0]["url"])
+        artistID = artist['id']
+
+        # Album Details
+        trackURIs = []
+        trackArt = []
+        z = 0
+
+        # Extract some album data
+        albumResults = spotifyObject.artist_albums(artist_id=artistID)
+        print(json.dumps(albumResults, sort_keys=True, indent=4))
+
+# End the program
+    if choice == "1":
+        break
+
+
+
