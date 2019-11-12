73eb9f7ed84e5fa7d7eb4241b1fa89a13ca89507
Gabriel Alexander Mongalo <gmongalo90@gmail.com>
2019-11-11 22:04:56 -0700

    updated spotify authentication

--- test_spotify_api.py
+++ test_spotify_api.py
@@ -1,4 +1,23 @@
+import os
+import sys
+import json
 import spotipy
-spotify = spotipy.Spotify()
-results = spotify.search(q='artist:' + name, type='artist')
-print results
+import webbrowser
+import spotipy.util as util
+from json.decoder import JSONDecodeError
+
+# Get the user name from terminal
+username = sys.argv[1]
+
+# userid = 1234983255
+
+# Erase cache and prompt user permission
+try:
+    token = util.prompt_for_user_token(username)
+except:
+    os.remove(f".cache-{username}")
+    token = util.prompt_for_user_token(username)
+
+# Create Spotify Object
+
+spotifyObject = spotipy.Spotify(auth=token)
